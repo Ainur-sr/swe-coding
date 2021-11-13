98eabdb0bbd91480d75a00c01ce4026e792834e6
ainur <a.shigapov@faberlic.com>
2021-11-14 02:34:32 +0300

    twosum1

--- src/com/coding/leetcode/containsduplicate2/Solution219first.java
+++ src/com/coding/leetcode/containsduplicate2/Solution219first.java
@@ -5,10 +5,10 @@ import java.util.*;
 public class Solution219first {
 
     public static void main(String[] args) {
-//        System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1}, 3)); //true
-//        System.out.println(containsNearbyDuplicate(new int[]{1,0,1,1}, 1)); //true
-//        System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1,2,3}, 2)); //false
-//        System.out.println(containsNearbyDuplicate(new int[]{1,2}, 2)); //false
+        System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1}, 3)); //true
+        System.out.println(containsNearbyDuplicate(new int[]{1,0,1,1}, 1)); //true
+        System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1,2,3}, 2)); //false
+        System.out.println(containsNearbyDuplicate(new int[]{1,2}, 2)); //false
         System.out.println(containsNearbyDuplicate(new int[]{0, 1, 2, 3, 4, 0, 0, 7, 8, 9, 10, 11, 12, 0}, 1)); //true
     }
 
---
+++ src/com/coding/leetcode/twosum1/Solution.java
@@ -0,0 +1,32 @@
+package com.coding.leetcode.twosum1;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9))); //[0,1]
+        System.out.println(Arrays.toString(twoSum(new int[]{3,2,4}, 6))); //[1,2]
+        System.out.println(Arrays.toString(twoSum(new int[]{3,3}, 6))); //[0,1]
+        System.out.println(Arrays.toString(twoSum(new int[]{-3,4,3,90}, 0))); //[0,2]
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        f:
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int sum = nums[i] + nums[j];
+                if (sum == target){
+                    result[0] = i;
+                    result[1] = j;
+                    break f;
+                }
+            }
+        }
+
+        return result;
+    }
+
+}
