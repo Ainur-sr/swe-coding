2d2833e4f97d2f82d643c7936bd4134a4190fd9b
ainur <a.shigapov@faberlic.com>
2021-11-15 02:43:26 +0300

    4. Median of Two Sorted Arrays

--- src/com/coding/leetcode/t4/SolutionTwo.java
+++ src/com/coding/leetcode/t4/SolutionTwo.java
@@ -1,7 +1,5 @@
 package com.coding.leetcode.t4;
 
-import java.util.*;
-
 public class SolutionTwo {
 
     public static void main(String[] args) {
@@ -15,54 +13,34 @@ public class SolutionTwo {
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         double result = 0.0;
-        List<Integer> integers = new ArrayList<>();
+        int[] integers = new int[nums1.length + nums2.length];
 
         int n1 = 0;
         int n2 = 0;
-        while (n1 < nums1.length || n2 < nums2.length) {
-            if (n1 < nums1.length && n2 < nums2.length) {
+        int x = 0;
+        while (n1 < nums1.length && n2 < nums2.length) {
                 if (nums1[n1] == nums2[n2]) {
-                    integers.add(nums1[n1]);
-                    integers.add(nums2[n2]);
-                    n1++;
-                    n2++;
+                    integers[x++] = nums1[n1++];
+                    integers[x++] = nums2[n2++];
                 } else if (nums1[n1] > nums2[n2]) {
-                    integers.add(nums2[n2]);
-                    n2++;
+                    integers[x++] = nums2[n2++];
                 } else if (nums1[n1] < nums2[n2]) {
-                    integers.add(nums1[n1]);
-                    n1++;
+                    integers[x++] = nums1[n1++];
                 }
-
-            } else if (n1 < nums1.length) {
-                for (int k = n1; k < nums1.length; k++) {
-                    integers.add(nums1[k]);
-                }
-                break;
-            } else if (n2 < nums2.length) {
-                for (int k = n2; k < nums2.length; k++) {
-                    integers.add(nums2[k]);
-                }
-                break;
-            } else {
-                break;
-            }
         }
 
-        int index = 0;
-        int mid = integers.size() / 2;
-        int prev = 0;
-        for (Integer integer : integers) {
-            if (index == mid) {
-                if (integers.size() % 2 == 1) {
-                    result = integer;
-                } else {
-                    result = (integer + prev) / 2.0;
-                }
-                break;
-            }
-            prev = integer;
-            index++;
+        while (n1 < nums1.length){
+            integers[x++] = nums1[n1++];
+        }
+        while (n2 < nums2.length){
+            integers[x++] = nums2[n2++];
+        }
+
+        int mid = integers.length / 2;
+        if (integers.length % 2 == 1) {
+            result = integers[mid];
+        } else {
+            result = (integers[mid] + integers[mid-1]) / 2.0;
         }
 
         return result;
