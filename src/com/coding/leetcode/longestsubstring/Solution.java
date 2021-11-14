74b8a6359317fb0490b09472732c8bef6b40600c
ainur <a.shigapov@faberlic.com>
2021-11-15 00:29:39 +0300

    3. Longest Substring Without Repeating Characters

---
+++ src/com/coding/leetcode/longestsubstring/Solution.java
@@ -0,0 +1,41 @@
+package com.coding.leetcode.longestsubstring;
+
+import java.util.List;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("dvdf")); //3
+        System.out.println(lengthOfLongestSubstring("abcabcbb")); //3
+        System.out.println(lengthOfLongestSubstring("pwwkew")); //3
+        System.out.println(lengthOfLongestSubstring("")); //0
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        StringBuilder sb = new StringBuilder();
+        int max = 0;
+        int startIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            String symb = s.substring(i, i + 1);
+
+            if (sb.length() == 0){
+                startIndex = i;
+            }
+
+            if (sb.toString().contains(symb)) {
+                sb.setLength(0);
+                i = startIndex;
+            } else {
+                sb.append(symb);
+            }
+
+            if (max < sb.length()) {
+                max = sb.length();
+            }
+        }
+
+        return max;
+    }
+
+}
