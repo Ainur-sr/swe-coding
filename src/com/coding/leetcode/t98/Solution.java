fda2612a0b99b68131e55dedd7f12505277c2dd9
ainur <ainur-sr@yandex.ru>
2023-12-17 09:39:57 +0300

    t98

--- src/com/coding/leetcode/t98/Solution.java
+++ src/com/coding/leetcode/t98/Solution.java
@@ -3,82 +3,30 @@ package com.coding.leetcode.t98;
 public class Solution {
 
     public static void main(String[] args) {
-        boolean test01 = isValidBST(test01());
-        boolean test02 = isValidBST(test02());
         boolean test03 = isValidBST(test03());
-//        System.out.println("test01 : " + test01);
-//        System.out.println("test02 : " + test02);
         System.out.println("test03 : " + test03);
     }
 
     public static boolean isValidBST(TreeNode root) {
-        if (root.left != null && root.left.val >= root.val) return false;
-        if (root.right != null && root.right.val <= root.val) return false;
-
-        boolean result = checkTree(root.left, root.val, true) && checkTree(root.right, root.val, false);
-        return result;
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    private static boolean checkTree(TreeNode node, int parentVal, boolean isLeft) {
+    public static boolean isValidBST(TreeNode node, long minVal, long maxVal) {
         if (node == null) return true;
-
-        boolean nodeLeftRes = true;
-        boolean nodeRightRes = true;
-
-        if (node.left != null) {
-            if (isLeft) {
-                if (!(node.left.val < parentVal && node.left.val < node.val)) return false;
-            } else {
-                if (!(node.left.val > parentVal && node.left.val < node.val)) return false;
-            }
-            nodeLeftRes = checkTree(node.left, node.val, true);
-        }
-        if (node.right != null) {
-            if (isLeft) {
-                if (!(node.right.val < parentVal && node.right.val < node.val)) return false;
-            } else {
-                if (!(node.right.val > parentVal && node.right.val > node.val)) return false;
-            }
-            nodeRightRes = checkTree(node.right, node.val, false);
-        }
-
-        return nodeLeftRes && nodeRightRes;
-    }
-
-
-    private static TreeNode test01() {
-        TreeNode r1_l1 = new TreeNode(3);
-        TreeNode r1_r1 = new TreeNode(6);
-
-        TreeNode l1 = new TreeNode(1);
-        TreeNode r1 = new TreeNode(4, r1_l1, r1_r1);
-
-        TreeNode root = new TreeNode(5, l1, r1);
-        return root;
-    }
-
-    private static TreeNode test02() {
-        TreeNode r1_l1 = new TreeNode(3);
-        TreeNode r1_r1 = new TreeNode(7);
-
-        TreeNode l1 = new TreeNode(4);
-        TreeNode r1 = new TreeNode(6, r1_l1, r1_r1);
-
-        TreeNode root = new TreeNode(5, l1, r1);
-        return root;
+        if (node.val <= minVal || node.val >= maxVal) return false;
+        return isValidBST(node.left, minVal, node.val) && isValidBST(node.right, node.val, maxVal);
     }
 
     private static TreeNode test03() {
-        TreeNode l1_l1 = new TreeNode(0);
-        TreeNode l1_r1 = new TreeNode(2);
+        TreeNode rootLeft_l1 = new TreeNode(50, new TreeNode(20), new TreeNode(55));
+        TreeNode rootLeft_r1 = new TreeNode(100, new TreeNode(75), new TreeNode(110));
+        TreeNode rootLeft = new TreeNode(70, rootLeft_l1, rootLeft_r1);
 
-        TreeNode r1_l1 = new TreeNode(4);
-        TreeNode r1_r1 = new TreeNode(6);
+        TreeNode rootRight_l1 = new TreeNode(130, new TreeNode(119), new TreeNode(135));
+        TreeNode rootRight_r1 = new TreeNode(160, new TreeNode(150), new TreeNode(200));
+        TreeNode rootRight = new TreeNode(140, rootRight_l1, rootRight_r1);
 
-        TreeNode l1 = new TreeNode(1, l1_l1, l1_r1);
-        TreeNode r1 = new TreeNode(5, r1_l1, r1_r1);
-
-        TreeNode root = new TreeNode(3, l1, r1);
+        TreeNode root = new TreeNode(120, rootLeft, rootRight);
         return root;
     }
 }
