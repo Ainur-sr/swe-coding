76b301598adbbc4569c82dceae39c4d4711706d5
ainur <ainur-sr@yandex.ru>
2024-08-01 21:39:10 -0600

    first-unique-character-in-a-string

---
+++ src/com/coding/leetcode/amazon/ArraysAndStrings/t387_FirstUniqueCharacter/Solution.java
@@ -0,0 +1,31 @@
+package com.coding.leetcode.amazon.ArraysAndStrings.t387_FirstUniqueCharacter;
+
+import java.util.HashMap;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.firstUniqChar("leetcode"));
+        System.out.println(solution.firstUniqChar("loveleetcode"));
+        System.out.println(solution.firstUniqChar("aabb"));
+    }
+
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            Integer count = map.get(c);
+            if (count != null && count == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
