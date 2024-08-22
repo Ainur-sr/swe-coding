6f82ceeb452a1d14f04a3a2e45faea56b6cd4472
ainur <ainur-sr@yandex.ru>
2024-08-22 01:12:18 -0600

    integer-to-roman

---
+++ src/com/coding/leetcode/amazon/ArraysAndStrings/t12_IntegerToRoman/Solution.java
@@ -0,0 +1,32 @@
+package com.coding.leetcode.amazon.ArraysAndStrings.t12_IntegerToRoman;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.intToRoman(3749)); //"MMMDCCXLIX"
+    }
+
+    static final int[] values = new int[]{1, 4, 5, 9,
+                                        10, 40, 50, 90,
+                                        100, 400, 500, 900,
+                                        1000};
+    
+    static final String[] romans = new String[]{"I", "IV", "V", "IX",
+                                                "X", "XL", "L", "XC",
+                                                "C", "CD", "D", "CM",
+                                                "M"};
+
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = values.length - 1; i >= 0 && num > 0; i--) {
+            while (num >= values[i]) {
+                num = num - values[i];
+                sb.append(romans[i]);
+            }
+        }
+
+        return sb.toString();
+    }
+}
