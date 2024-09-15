020a80766f99381803c8849755951de84d193d80
ainur <ainur-sr@yandex.ru>
2024-09-15 02:50:45 -0600

    merge-k-sorted-lists

---
+++ src/com/coding/leetcode/amazon/linkedList/t23_MergekSortedLists/ListNode.java
@@ -0,0 +1,19 @@
+package com.coding.leetcode.amazon.linkedList.t23_MergekSortedLists;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
