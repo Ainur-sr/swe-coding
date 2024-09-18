e98f411af559f3c7dd8c8b44bfd53daf6f73c792
ainur <ainur-sr@yandex.ru>
2024-09-17 22:27:50 -0600

    linked-list-cycle

---
+++ src/com/coding/leetcode/amazon/sub/t141_LinkedListCycle/ListNode.java
@@ -0,0 +1,11 @@
+package com.coding.leetcode.amazon.sub.t141_LinkedListCycle;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
