8f090ad1993c886acf5cc33b316845030f596317
ainur <ainur-sr@yandex.ru>
2024-09-19 22:34:09 -0600

    search-in-rotated-sorted-array

---
+++ src/com/coding/leetcode/amazon/sortingAndSearching/t33_SearchInRotatedSortedArray/Solution.java
@@ -0,0 +1,38 @@
+package com.coding.leetcode.amazon.sortingAndSearching.t33_SearchInRotatedSortedArray;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = (l + (r - l)) / 2;
+            if (target == nums[mid]) return mid;
+
+            // left sorted portion
+            if (nums[l] <= nums[mid]) {
+                if (target > nums[mid] || target < nums[l]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            // right sorted portion
+            else {
+                if (target < nums[mid] || target > nums[r]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+}
