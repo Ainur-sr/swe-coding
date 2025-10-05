101064437d4f9b7ebdaa7f42710f4e35b8ac2a63
Ainur Shigapov <ainursh@amazon.com>
2025-10-04 23:33:50 -0600

    300. Longest Increasing Subsequence

--- src/com/coding/leetcode/t300/Solution.java
+++ src/com/coding/leetcode/t300/Solution.java
@@ -1,47 +1,44 @@
 package com.coding.leetcode.t300;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
     public int lengthOfLIS(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
-
-        int[] cache = new int[nums.length];
-        int cacheSize = 0;
+        List<Integer> cache = new ArrayList<>();
 
         for (int num : nums) {
             int bigIndex = findMinBiggerElement(cache, num, cacheSize);
             if (bigIndex == -1) {
-                cache[cacheSize] = num;
-                cacheSize++;
+                cache.add(num)
             } else {
-                cache[bigIndex] = num;
+                cache.set(bigIndex, num);
             }
         }
 
-        return cacheSize;
+        return cache.size();
     }
 
-    // Возвращает индекс первого элемента > target в массиве длины size.
+    // Возвращает индекс первого элемента > target
     // Если не найден — возвращает -1.
-    private int findMinBiggerElement(int[] arr, int target, int size) {
+    private int findMinBiggerElement(List<Integer> list, int target) {
         int left = 0;
-        int right = size - 1;
+        int right = list.size() - 1;
+        int res = -1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
+            if (list.get(mid) >= target) {
                 right = mid - 1;
+                res = mid;
+            } else {
+                left = mid + 1;
             }
         }
 
-        if (left >= size) {
-            return -1;
-        }
-        return left;
+        return res;
     }
-
-
 }
