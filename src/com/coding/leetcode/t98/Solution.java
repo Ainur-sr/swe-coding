000cff216141f9b5a6e332ace0dc94ebdaf62453
Ainur Shigapov <ainursh@amazon.com>
2026-02-03 23:53:16 -0600

    98. Validate Binary Search Tree

--- src/com/coding/leetcode/t98/Solution.java
+++ src/com/coding/leetcode/t98/Solution.java
@@ -2,31 +2,20 @@ package com.coding.leetcode.t98;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        boolean test03 = isValidBST(test03());
-        System.out.println("test03 : " + test03);
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, null, null);
     }
 
-    public static boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
+    private boolean validate(TreeNode node, Integer min, Integer max) {
+        if (node == null) {
+            return true;
+        }
 
-    public static boolean isValidBST(TreeNode node, long minVal, long maxVal) {
-        if (node == null) return true;
-        if (node.val <= minVal || node.val >= maxVal) return false;
-        return isValidBST(node.left, minVal, node.val) && isValidBST(node.right, node.val, maxVal);
-    }
+        if ((min != null && node.val <= min) || (max != null && node.val >= max)) {
+            return false;
+        }
 
-    private static TreeNode test03() {
-        TreeNode rootLeft_l1 = new TreeNode(50, new TreeNode(20), new TreeNode(55));
-        TreeNode rootLeft_r1 = new TreeNode(100, new TreeNode(75), new TreeNode(110));
-        TreeNode rootLeft = new TreeNode(70, rootLeft_l1, rootLeft_r1);
-
-        TreeNode rootRight_l1 = new TreeNode(130, new TreeNode(119), new TreeNode(135));
-        TreeNode rootRight_r1 = new TreeNode(160, new TreeNode(150), new TreeNode(200));
-        TreeNode rootRight = new TreeNode(140, rootRight_l1, rootRight_r1);
-
-        TreeNode root = new TreeNode(120, rootLeft, rootRight);
-        return root;
+        return validate(node.left, min, node.val) &&
+                validate(node.right, node.val, max);
     }
 }
